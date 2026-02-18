570b89d01195ea87d5608721845f342819293f85
Nishal Barman <32856353+nishalbarman@users.noreply.github.com>
2026-02-18 16:35:17 +0530

    changes in package.json of server to adapt vercel

--- apps/server/tsup.config.js
+++ apps/server/tsup.config.js
@@ -8,6 +8,6 @@ export const tsup = {
   skipNodeModulesBundle: false,
   entryPoints: ["src/index.js"],
   watch: false,
-  outDir: "api",
+  outDir: "dist",
   entry: ["./**/*.js"], //include all files under src
 };
